3d32df66fbe1afd916024ab77473f78fde864d2b
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-06-19 18:21:25 +0900

    feat : 몬스터 생성 구현
    MonsterSpawnArea 지역을 정해 랜덤으로 위치를 받아와 SpawnDelay 시간마다 몬스터 생성.

--- Assets/Scripts/Monster/MonsterController.cs
+++ Assets/Scripts/Monster/MonsterController.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 
 public class MonsterController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;            // 몬스터 이동속도
-    [SerializeField] private Rigidbody2D player;
-
+    private float moveSpeed = 1f;            // 몬스터 이동속도
+    
     private Rigidbody2D rb;
+    private Rigidbody2D target;
+
     private SpriteRenderer render;
+    private bool isAlive = true;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         render = GetComponent<SpriteRenderer>();
+        target = GameObject.Find("Player").GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
@@ -25,13 +28,13 @@ public class MonsterController : MonoBehaviour
     private void LateUpdate()
     {
         // 몬스터의 x축 값이 플레이어의 x축 값보다 작을 경우 flipX 가 true, 플레이어 방향을 보도록 함
-        render.flipX = player.position.x > rb.position.x;
+        render.flipX = target.position.x > rb.position.x;
     }
 
     private void Chase()
     {
         // monster에서 player로 가는 방향을 구하고 플레이어의 방향으로 지속적으로 이동
-        Vector2 dirVec = player.position - rb.position;
+        Vector2 dirVec = target.position - rb.position;
         rb.MovePosition(rb.position + dirVec.normalized * moveSpeed * Time.fixedDeltaTime);
     }
 }
---
+++ Assets/Scripts/Monster/MonsterSpawn.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawn : MonoBehaviour
+{
+    [SerializeField] float spawnDelay;          // 몬스터 생성 딜레이
+    [SerializeField] GameObject enemyPrefab;    // 생성 몬스터 프리팹
+
+    private BoxCollider2D area; // 몬스터 생성 범위
+    public List<GameObject> monsterList = new List<GameObject>();  // 몬스터 리스트
+
+    private void Awake()
+    {
+        area = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            // Vector3 값인 랜덤 위치를 받아옴
+            Vector3 spawnPos = GetRandomPosition();
+
+            // 설정된 enemyPrefab을 랜덤 spawnPos에 생성후 리스트에 추가. spawnDelay 시간이 지난 후 반복
+            GameObject instance = GameManager.Resource.Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+            monsterList.Add(instance);
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    private Vector2 GetRandomPosition()
+    {
+        Vector2 basePoint = transform.position; // 오브젝트 위치
+        Vector2 size = area.size;   // Rigidbody2D 의 사이즈
+
+        //x, y축 랜덤 좌표 얻기
+        float posX = basePoint.x + Random.Range(-size.x / 2f, size.x / 2f);
+        float posY = basePoint.y + Random.Range(-size.y / 2f, size.y / 2f);
+
+        Vector2 spawnPos = new Vector2(posX, posY);
+
+        return spawnPos;
+    }
+}
