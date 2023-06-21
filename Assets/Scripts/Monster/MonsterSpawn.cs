cff114b9be4eb80879adddecd82b1a7a245678d9
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-06-21 18:06:36 +0900

    fix : MonsterSpawnPoint 위치 랜덤
    spawnPoints[(Random.Range(0, 8))] 로 위치를 받아와서 그 위치에 리스폰

--- Assets/Scripts/Monster/MonsterSpawn.cs
+++ Assets/Scripts/Monster/MonsterSpawn.cs
@@ -6,13 +6,14 @@ public class MonsterSpawn : MonoBehaviour
 {
     [SerializeField] float spawnDelay;          // 몬스터 생성 딜레이
     [SerializeField] GameObject[] spawnPrefabs; // 스폰 몬스터 프리팹들
-    [SerializeField] Transform[] spawnPoints;
+    [SerializeField] Transform[] spawnPoints;   // 스폰 위치들
 
     public List<GameObject> monsters = new List<GameObject>();  // 몬스터 리스트
     private int poolSize = 10;
 
     private void Start()
     {
+        // poolSize만큼 리스트를 담은 후 스폰
         for(int i = 0; i < poolSize; i++)
         {
             monsters.Add(GameManager.Pool.Get(spawnPrefabs[i]));
@@ -40,7 +41,7 @@ public class MonsterSpawn : MonoBehaviour
                     continue;
 
                 // Vector3 값인 위치를 받아옴
-                Vector3 spawnPos = spawnPoints[i].position;
+                Vector3 spawnPos = GetSpawnPos();
 
                 // 몬스터의 위치를 spawnPos로 설정 후 활성화
                 monsters[i].transform.position = spawnPos;
@@ -50,5 +51,11 @@ public class MonsterSpawn : MonoBehaviour
                 break;
             }
         }
+    }
+
+    // SpawnPoint 0~7 까지 중 랜덤으로 위치 받아옴
+    private Vector2 GetSpawnPos()
+    {
+        return spawnPoints[(Random.Range(0, 8))].position;
     }
 }
