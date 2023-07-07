f531bdc2443d1d23e4c237d1e426c82d99c9d71f
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-07-07 21:50:20 +0900

    fix : 파워업 수치 적용 버그 수정

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -17,6 +17,7 @@ public class DataManager : MonoBehaviour
     public ItemData fireData;
 
     public float gameTime;
+
     public float volume;
     public bool isMute;
     public bool isPrintDamage;
